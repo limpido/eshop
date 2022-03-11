5007c29a6b6dcb95356bbbecd1ac3436e99c2027
limpido <jiichuan0325@gmail.com>
2022-03-11 21:36:52 +0800

    db: update cart.sql

--- database-scripts/cart.sql
+++ database-scripts/cart.sql
@@ -2,13 +2,13 @@ CREATE DATABASE IF NOT EXISTS gamelib; -- Creates only if it does not exists
 SHOW DATABASES;
 USE gamelib;
 
-drop table if exists Cart;
+drop table if exists cart;
 
 CREATE TABLE IF NOT EXISTS cart (
-    uid INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(50) NOT NULL,
-    qty INT NOT NULL,
-    totalCost DECIMAL(10, 2) NOT NULL
+    uid INT NOT NULL,
+    gameId INT NOT NULL,
+    qtyOrdered INT NOT NULL,
+    PRIMARY KEY (uid, gameId)
 );
 
 select * from Cart;
