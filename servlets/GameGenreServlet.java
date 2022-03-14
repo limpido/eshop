b1a242f6645aca11997d306d2589f0a182e239e2
Eternalmadhatter <97937707+Eternalmadhatter@users.noreply.github.com>
2022-03-14 23:44:55 +0800

    angular

--- servlets/GameGenreServlet.java
+++ servlets/GameGenreServlet.java
@@ -10,12 +10,17 @@ public class GameGenreServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse res)
     throws ServletException, IOException {
+        res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.setHeader("Access-Control-Max-Age", "3600");
         DbConnection db = new DbConnection();
         Helper helper = new Helper();
         try (
             Connection conn = db.connect();         
             ) {
-            helper.addHeader(res);
+            //helper.addHeader(res);
             res.setContentType("application/json");
             res.setCharacterEncoding("UTF-8");
             PrintWriter out = res.getWriter();
